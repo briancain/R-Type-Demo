290d072167b578c1a8f852f53dd30230c49aa35f
Brian <brianccain@gmail.com>
2013-07-21 11:06:58 -0700

    (add) Add powerup file

---
+++ src/rtype/PowerUp.java
@@ -0,0 +1,46 @@
+package rtype;
+
+import java.awt.Image;
+
+/**
+ * 
+ * @author brian
+ *
+ */
+public class PowerUp implements Being {
+	
+	public PowerUp(){
+		
+	}
+
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean isVisible() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void setVisible(boolean visible) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Image getImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
